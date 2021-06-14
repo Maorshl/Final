dedd99e86b40e11d8f254ba628618a2d6bb61dac
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-06-14 19:44:30 +0300

    Created login page

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import "../Style/App.css";
-
+import AppBar from "./AppBar";
+import SignIn from "./SignIn";
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {user && <AppBar />}
+      {!user && <SignIn setUser={setUser} />}
     </div>
   );
 }
