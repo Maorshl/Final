0e36d013a550ed7853f142186699dcfcb3fabe0f
gil6464 <gil6464@gmail.com>
2021-06-20 14:47:06 +0300

    Update SignUp.js

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import axios from "axios";
 import { Redirect } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import axios from "axios";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Container,
+  Typography,
+} from "@material-ui/core";
 
 function Copyright() {
   return (
@@ -40,7 +42,7 @@ function Copyright() {
   );
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
@@ -68,7 +70,7 @@ export default function SignUp() {
   const [redirect, setRedirect] = useState(false);
   const [tryAgain, setTryAgain] = useState(false);
 
-  const createUser = async (e) => {
+  const createUser = async e => {
     e.preventDefault();
     try {
       const response = await axios.post("http://localhost:8080/user/create", {
@@ -100,7 +102,7 @@ export default function SignUp() {
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
-                onChange={(e) => {
+                onChange={e => {
                   setUsername(e.target.value);
                 }}
                 autoComplete="Uname"
@@ -115,7 +117,7 @@ export default function SignUp() {
             </Grid>
             <Grid item xs={12}>
               <TextField
-                onChange={(e) => {
+                onChange={e => {
                   setEmail(e.target.value);
                 }}
                 variant="outlined"
@@ -129,7 +131,7 @@ export default function SignUp() {
             </Grid>
             <Grid item xs={12}>
               <TextField
-                onChange={(e) => {
+                onChange={e => {
                   setPassword(e.target.value);
                 }}
                 variant="outlined"
