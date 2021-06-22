214a52794c2f4c362aaba283c4191ce7853be909
gil6464 <gil6464@gmail.com>
2021-06-22 20:40:35 +0300

    Update FloatingButton.js
    moved the style from FloatingButton into style file

--- client/src/Components/FloatingButton.js
+++ client/src/Components/FloatingButton.js
@@ -1,27 +1,14 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import { Fab, Link } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    marginBottom: "1rem",
-    marginLeft: "1rem",
-    position: "fixed",
-    bottom: 0,
-    zIndex: 9000,
-  },
-  extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
-}));
+import useStyles from "../Style";
 
 export default function FloatingActionButtons() {
   const classes = useStyles();
 
   return (
     <Link href="/addPost">
-      <div className={classes.root}>
+      <div className={classes.rootFloating}>
         <Fab color="primary" aria-label="add">
           <AddIcon />
         </Fab>
