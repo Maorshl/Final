15849cc196e2bbeb0560d64269e4fe34af0cc3d6
gil6464 <gil6464@gmail.com>
2021-06-29 18:50:43 +0300

    added search component and is style

--- client/src/Components/Search.js
+++ client/src/Components/Search.js
@@ -1,37 +1,80 @@
-import React from "react";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormLabel from "@material-ui/core/FormLabel";
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
+import StyledMenuItem from "../Style/StyledMenuItem";
+import StyledMenu from "../Style/StyledMenu";
 
-function Search(props) {
-  const [value, setValue] = React.useState("female");
+function Search() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [searchFilter, setSearchfilter] = useState(undefined);
 
-  const handleChange = event => {
-    setValue(event.target.value);
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
   };
 
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <FormControl component="fieldset">
-      <FormLabel component="legend">Gender</FormLabel>
-      <RadioGroup
-        aria-label="gender"
-        name="gender1"
-        value={value}
-        onChange={handleChange}
+    <div>
+      <Button
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
       >
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
-        <FormControlLabel value="other" control={<Radio />} label="Other" />
-        <FormControlLabel
-          value="disabled"
-          disabled
-          control={<Radio />}
-          label="(Disabled option)"
-        />
-      </RadioGroup>
-    </FormControl>
+        Search By : {searchFilter}
+      </Button>
+      <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem>
+          <ListItemIcon>
+            <SendIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText
+            primary="Title"
+            onClick={() => {
+              setSearchfilter("Title");
+              handleClose();
+            }}
+          />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <DraftsIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText
+            primary="Content"
+            onClick={() => {
+              setSearchfilter("Content");
+              handleClose();
+            }}
+          />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <InboxIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText
+            primary="Tags"
+            onClick={() => {
+              setSearchfilter("Tags");
+              handleClose();
+            }}
+          />
+        </StyledMenuItem>
+      </StyledMenu>
+    </div>
   );
 }
 
