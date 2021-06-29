a748fe7901c33436e0ef3135542205d374f505ee
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-06-29 09:49:24 +0300

    Create NeedToLogin.js

---
+++ client/src/Components/NeedToLogin.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NeedToLogin(props) {
+  return (
+    <div>
+      <h1>Hi!</h1>
+      <h2>Sorry but to see this page you have to login...</h2>
+    </div>
+  );
+}
+
+export default NeedToLogin;
