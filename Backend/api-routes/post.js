94630dc690f29089232c5a929a8620f2f37d64fa
gil6464 <gil6464@gmail.com>
2021-07-01 21:53:53 +0300

    search works at myposts

--- Backend/api-routes/post.js
+++ Backend/api-routes/post.js
@@ -14,7 +14,7 @@ post.post("/create", validateToken, (req, res) => {
   }
 });
 
-post.get("/:user/private", validateToken, (req, res) => {
+post.get("/private", validateToken, (req, res) => {
   try {
     getPrivatePosts(req, res);
   } catch (error) {
--- client/src/Components/MyPosts.js
+++ client/src/Components/MyPosts.js
@@ -4,16 +4,30 @@ import Cookies from "js-cookie";
 import AppBar from "./AppBar";
 import PostCard from "./PostCard";
 import { Typography } from "@material-ui/core";
+import Search from "./Search";
 
 function MyPosts({ setUser }) {
   const [posts, setPosts] = useState([]);
+  const [searchFilter, setSearchfilter] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [showRefresh, setShowRefresh] = useState(false);
 
+  const serach = () => {
+    //* If no data to search=> retrun
+    if ((!searchFilter && !searchText) || (searchText && !searchFilter)) return;
+    if (!searchText && searchFilter) {
+      setSearchfilter("");
+      setShowRefresh(false);
+    }
+    getData();
+  };
+  async function getData() {
+    const privatePosts = await getPrivatePosts(searchFilter, searchText);
+    setPosts(privatePosts);
+    console.log(privatePosts);
+  }
   useEffect(() => {
-    (async function () {
-      const privatePosts = await getPrivatePosts();
-      setPosts(privatePosts);
-      console.log(privatePosts);
-    })();
+    getData();
   }, []);
 
   return (
@@ -22,18 +36,27 @@ function MyPosts({ setUser }) {
       <Typography variant="h2" color="primary">
         My Posts
       </Typography>
+      <Search
+        setSearchfilter={setSearchfilter}
+        setSearchText={setSearchText}
+        searchFilter={searchFilter}
+        serach={serach}
+        searchText={searchText}
+        showRefresh={showRefresh}
+        setShowRefresh={setShowRefresh}
+      />
       {posts &&
-        posts.map((post) => {
+        posts.map(post => {
           return <PostCard post={post} />;
         })}
     </div>
   );
 }
 
-async function getPrivatePosts() {
+async function getPrivatePosts(searchFilter, searchText) {
   const userName = Cookies.get("userName");
   const { data } = await axios.get(
-    `http://localhost:8080/post/${userName}/private`
+    `http://localhost:8080/post/private?userName=${userName}&searchFilter=${searchFilter}&searchText=${searchText}`
   );
   return data;
 }
