b86196820da1d07da00416640b10393730ee5ea9
gil6464 <gil6464@gmail.com>
2021-07-01 19:28:53 +0300

    Update getPrivatePosts.js
    new posts will come insted of old posts

--- Backend/services/getPrivatePosts.js
+++ Backend/services/getPrivatePosts.js
@@ -2,8 +2,11 @@ const Post = require("../models/Post");
 
 function getPrivatePosts(req, res) {
   const { user } = req.params;
-  Post.find({ author: user, private: true }).then((result) => {
-    res.json(result);
-  });
+  Post.find({ author: user, private: true })
+    .sort({ createdAt: "desc" })
+    .limit(50)
+    .then(result => {
+      res.json(result);
+    });
 }
 module.exports = getPrivatePosts;
