19d21b97fabd4984495c59ed2ff7c4d30da3a9b0
gil6464 <gil6464@gmail.com>
2021-07-01 18:42:12 +0300

    Update GetTitle.js

--- Backend/services/GetTitle.js
+++ Backend/services/GetTitle.js
@@ -1,6 +1,8 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
+//*This function go to the URL and scrape the <title> from the HTML
+
 async function getTitle(url) {
   try {
     const page = await axios.get(url);
