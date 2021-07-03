89523a4e5615c1929161b4f888f2fa825d23b165
gil6464 <gil6464@gmail.com>
2021-07-04 01:39:18 +0300

    Update SavedPosts.js
    search feature works on savedPosts

--- client/src/Components/SavedPosts.js
+++ client/src/Components/SavedPosts.js
@@ -4,36 +4,62 @@ import Cookies from "js-cookie";
 import AppBar from "./AppBar";
 import PostCard from "./PostCard";
 import { Typography } from "@material-ui/core";
+import Search from "./Search";
 
 function MyPosts({ setUser }) {
   const [posts, setPosts] = useState([]);
+  const [searchFilter, setSearchfilter] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [showRefresh, setShowRefresh] = useState(false);
+
+  const serach = () => {
+    //* If no data to search => retrun
+    if ((!searchFilter && !searchText) || (searchText && !searchFilter)) return;
+    if (!searchText && searchFilter) {
+      setSearchfilter("");
+      setShowRefresh(false);
+    }
+    getData();
+  };
+
+  async function getData() {
+    const privatePosts = await getPrivatePosts(searchFilter, searchText);
+    setPosts(privatePosts);
+  }
 
   useEffect(() => {
-    (async function () {
-      const privatePosts = await getPrivatePosts();
-      setPosts(privatePosts);
-    })();
+    getData();
   }, []);
 
   return (
     <div>
       <AppBar setUser={setUser} />
       <Typography variant="h2" color="primary">
-        My Posts
+        Saved Posts
       </Typography>
+      <Search
+        setSearchfilter={setSearchfilter}
+        setSearchText={setSearchText}
+        searchFilter={searchFilter}
+        serach={serach}
+        searchText={searchText}
+        showRefresh={showRefresh}
+        setShowRefresh={setShowRefresh}
+      />
       {posts &&
-        posts.map((post) => {
+        posts.map(post => {
           return <PostCard post={post} />;
         })}
     </div>
   );
 }
 
-async function getPrivatePosts() {
+async function getPrivatePosts(searchFilter, searchText) {
   const userName = Cookies.get("userName");
-  const { data } = await axios.post(`http://localhost:8080/post/savedposts`, {
-    userName,
-  });
+  const { data } = await axios.get(
+    `http://localhost:8080/post/savedposts?userName=${userName}&searchFilter=${searchFilter}&searchText=${searchText}`,
+    {}
+  );
   return data;
 }
 export default MyPosts;
