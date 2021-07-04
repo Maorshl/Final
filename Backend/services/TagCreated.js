e178d535f601697cbfe8e4a33eddf59e18c19139
gil6464 <gil6464@gmail.com>
2021-07-04 14:53:14 +0300

    Update TagCreated.js
    fixed createTag function

--- Backend/services/TagCreated.js
+++ Backend/services/TagCreated.js
@@ -1,21 +1,38 @@
 const Tag = require("../models/Tag");
 
-//* This function recive array of tags that just writed in a posts, and create them in need.
+//* This function recive array of tags that just writed in a posts, and create tag if need.
+//* Else, add to array to send notifications, new tag dosent have followers..
 
 async function createTag(tags) {
-  tags.forEach(async tag => {
-    const checkTag = await Tag.find({ name: tag });
-    if (checkTag.length === 0) {
-      const createNewTag = {
-        name: tag,
-        followers: [],
-      };
-      const newTag = new Tag(createNewTag);
-      newTag.save();
-    } else {
-      console.log("exist");
-    }
+  const tagsToSend = [];
+  const requests = tags.map(tag => {
+    return new Promise(async resolve => {
+      const checkTag = await Tag.find({ name: tag });
+      if (checkTag.length === 0) {
+        const createNewTag = {
+          name: tag,
+          followers: [],
+        };
+        const newTag = new Tag(createNewTag);
+        newTag.save();
+        resolve(tag);
+      } else {
+        tagsToSend.push(tag);
+        resolve(tag);
+      }
+    });
   });
+  Promise.all(requests).then(result => console.log(tagsToSend));
 }
+
+// function sendNofitication() {
+//   allFollowers = [];
+//   //* Get all followers into list.
+//   tagsToSend.forEach(async tag => {
+//     const eachTag = await Tag.find({ name: tag });
+//     allFollowers.push(...eachTag[0].followers);
+//   });
+//   console.log(allFollowers);
+// }
 
 module.exports = createTag;
