73a1c8603e2d61f85864b9b9c87c55241bfb4fd6
gil6464 <gil6464@gmail.com>
2021-07-05 00:32:21 +0300

    Update GetNotifications.js

--- Backend/services/GetNotifications.js
+++ Backend/services/GetNotifications.js
@@ -3,7 +3,6 @@ const User = require("../models/User");
 async function getUserNotifications(req, res) {
   const { userName } = req.query;
   const user = await User.findOne({ userName }).exec();
-  console.log(user);
   res.send(user.notifications);
 }
 
