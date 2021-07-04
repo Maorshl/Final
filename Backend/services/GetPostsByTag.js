2c9bd554ea6aa25d76e99ed4630998c519239563
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-07-04 13:24:57 +0300

    Update GetPostsByTag.js

--- Backend/services/GetPostsByTag.js
+++ Backend/services/GetPostsByTag.js
@@ -2,9 +2,14 @@ const Post = require("../models/Post");
 
 async function getPostsByTag(req, res) {
   const { tag } = req.params;
+  let { searchFilter, searchText } = req.query;
+
+  const text = new RegExp(searchText, "i");
+  searchFilter ? searchFilter : (searchFilter = "title");
 
   const postsByTags = await Post.find({
     tags: { $in: [tag] },
+    $and: [{ [searchFilter]: text }, { private: false }],
   })
     .sort({ createdAt: "desc" })
     .limit(50);
