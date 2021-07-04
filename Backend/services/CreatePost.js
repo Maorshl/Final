73123792cce6aad7bc611cc2e11fcf9d66dbffbd
gil6464 <gil6464@gmail.com>
2021-07-04 11:07:02 +0300

    Update CreatePost.js
    wrote the functions names that need for notifications

--- Backend/services/CreatePost.js
+++ Backend/services/CreatePost.js
@@ -4,6 +4,8 @@ const getTitle = require("./GetTitle");
 async function createPost(post) {
   try {
     const newPost = new Post(post);
+    // postCreated(newPost.tags);
+    // sendNofitication(newPost.author,newPost.tags);
     newPost.head = await getTitle(newPost.url);
     newPost.save();
     return "Post created successfully";
