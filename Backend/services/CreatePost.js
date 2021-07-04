7b58a76f595ff83cf44e53d6af9e76beead700c9
gil6464 <gil6464@gmail.com>
2021-07-04 17:13:14 +0300

    Update CreatePost.js
    every posts that created send data for functions that create notifications

--- Backend/services/CreatePost.js
+++ Backend/services/CreatePost.js
@@ -1,13 +1,14 @@
 const Post = require("../models/Post");
 const getTitle = require("./GetTitle");
+const postCreated = require("./PostCreated");
 
 async function createPost(post) {
   try {
     const newPost = new Post(post);
-    // postCreated(newPost.tags);
-    // sendNofitication(newPost.author,newPost.tags);
     newPost.head = await getTitle(newPost.url);
-    newPost.save();
+    newPost.save().then(result => {
+      postCreated(result);
+    });
     return "Post created successfully";
   } catch (error) {
     return error;
