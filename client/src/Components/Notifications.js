11b98ca6e7e455b40553c3f58e37f47cbe67e737
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-07-05 10:59:12 +0300

    Update Notifications.js

--- client/src/Components/Notifications.js
+++ client/src/Components/Notifications.js
@@ -1,8 +1,13 @@
 import React from "react";
+import StyledMenuItem from "../Style/StyledMenuItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
-function Notifications(post) {
-  console.log("post");
-  return <div>"alert"</div>;
+function Notifications({ notification }) {
+  return (
+    <StyledMenuItem>
+      <ListItemText primary={"notification.post.title"} secondary={"tag"} />
+    </StyledMenuItem>
+  );
 }
 
 export default Notifications;
