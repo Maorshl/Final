ffc83ab512f48d58d188f70376dfc47fb96db4a5
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-07-05 18:21:35 +0300

    Update Notifications.js

--- client/src/Components/Notifications.js
+++ client/src/Components/Notifications.js
@@ -1,12 +1,26 @@
 import React from "react";
 import StyledMenuItem from "../Style/StyledMenuItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import { ListItemText, Link } from "@material-ui/core";
+import LinkIcon from "@material-ui/icons/Link";
 
 function Notifications({ notification }) {
+  console.log(notification);
   return (
-    <StyledMenuItem>
-      <ListItemText primary={"notification.post.title"} secondary={"tag"} />
-    </StyledMenuItem>
+    <div
+      className={notification.read ? "notificationRead" : "notificationUnread"}
+    >
+      <StyledMenuItem>
+        New Post from: {notification.post.author}
+        <ListItemText
+          primary={notification.post.title}
+          secondary={
+            <Link href={notification.post.url} target="_blank">
+              <LinkIcon />
+            </Link>
+          }
+        />
+      </StyledMenuItem>
+    </div>
   );
 }
 
