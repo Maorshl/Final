bfdc08f5b36161b8a8a922f62e55085935e5bb7a
gil6464 <gil6464@gmail.com>
2021-07-07 16:57:45 +0300

    Update rating.js

--- Backend/api-routes/rating.js
+++ Backend/api-routes/rating.js
@@ -5,22 +5,28 @@ const getRaters = require("../services/GetRaters");
 const ratePost = require("../services/RatePost");
 const getAvgRate = require("../services/GetAvgRate");
 
-rating.get("/isRated", validateToken, async (req, res) => {
-  const { id, userName } = req.query;
-  const data = await getRaters(id, userName);
-  res.send(data);
+rating.get("/isRated", validateToken, (req, res) => {
+  try {
+    getRaters(req, res);
+  } catch (error) {
+    res.send(error.message).status(500);
+  }
 });
 
 rating.post("/ratePost", validateToken, async (req, res) => {
-  const { postId, userName, rate } = req.body;
-  const AVG = await ratePost(postId, userName, rate);
-  res.status(200).send({ AVG });
+  try {
+    ratePost(req, res);
+  } catch (error) {
+    res.send(error.message).status(500);
+  }
 });
 
 rating.get("/getRate", async (req, res) => {
-  const { id } = req.query;
-  const AVG = await getAvgRate(id);
-  res.json({ AVG });
+  try {
+    getAvgRate(req, res);
+  } catch (error) {
+    res.send(error.message).status(500);
+  }
 });
 
 module.exports = rating;
