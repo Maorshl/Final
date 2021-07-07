9f06e46a88b3533eeb077349a8870331a86f6168
gil6464 <gil6464@gmail.com>
2021-07-07 16:58:29 +0300

    Update GetRaters.js

--- Backend/services/GetRaters.js
+++ Backend/services/GetRaters.js
@@ -2,12 +2,13 @@ const Post = require("../models/Post");
 
 //* This function receive post id and user name, and return true/false if the user rated the post.
 
-async function getRaters(id, userName) {
+async function getRaters(req, res) {
+  const { id, userName } = req.query;
   const post = await Post.findById(id);
   const { raters } = post;
   const checkUser = raters.find(rater => rater === userName);
-  if (checkUser) return true;
-  return false;
+  if (checkUser) return res.send(true);
+  return res.send(false);
 }
 
 module.exports = getRaters;
