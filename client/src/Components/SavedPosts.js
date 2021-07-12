dcb26fe3331c0ec83cc29e5872d7e30b031609d3
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-07-12 23:33:41 +0300

    Update SavedPosts.js

--- client/src/Components/SavedPosts.js
+++ client/src/Components/SavedPosts.js
@@ -20,7 +20,7 @@ function MyPosts({ setUser }) {
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const correctPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
-  const paginate = pageNumber => {
+  const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
@@ -61,7 +61,7 @@ function MyPosts({ setUser }) {
         setShowRefresh={setShowRefresh}
       />
       {correctPosts &&
-        correctPosts.map(post => {
+        correctPosts.map((post) => {
           return <PostCard post={post} />;
         })}
       <Pagination
@@ -77,7 +77,7 @@ function MyPosts({ setUser }) {
 async function getPrivatePosts(searchFilter, searchText) {
   const userName = Cookies.get("userName");
   const { data } = await axios.get(
-    `http://ec2-3-80-252-156.compute-1.amazonaws.com:8080/post/savedposts?userName=${userName}&searchFilter=${searchFilter}&searchText=${searchText}`,
+    `http://localhost:8080/post/savedposts?userName=${userName}&searchFilter=${searchFilter}&searchText=${searchText}`,
     {}
   );
   return data;
